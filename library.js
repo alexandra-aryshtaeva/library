517a9c5f30c77747bcc5057475340b9294982286
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2023-12-18 19:06:44 +0000

    stuff

--- library.js
+++ library.js
@@ -1,6 +1,11 @@
-const input = document.querySelector(input);
+const nameInput = document.querySelector("#book_name");
+const authorInput = document.querySelector("#book_author");
+const pagesInput = document.querySelector("#book_pages");
+let addBookBtn = document.querySelector("add_book_btn");
 
-const myLibrary = [book1];
+// ________________________________________
+
+const myLibrary = [];
 
 function Book(name, author, pages) {
   this.name = name;
@@ -11,4 +16,41 @@ function Book(name, author, pages) {
 // examaple
 const book1 = new Book("The Hobiit", "J.R.R. Tolkien", "320");
 
-function addBook() {}
+// addBookBtn.addEventListener("click", () => {
+//   book.innerHTML = "ndkjsa";
+// });
+
+function addBook() {
+  myLibrary.push(
+    new Book(nameInput.value, authorInput.value, pagesInput.value)
+  );
+
+  console.log(myLibrary);
+
+  renderBooks();
+}
+
+function removeBook() {
+  console.log(myLibrary);
+}
+
+function renderBooks() {
+  const library = document.querySelector(".library");
+
+  // library.innerHTML = "";
+
+  // // book.innerHTML = `
+  // //   <p> Book name: ${nameInput.value}</p>
+  // // <p> Author name: ${authorInput.value}</p>
+  // // <p> Number of pages: ${pagesInput.value}</p>
+  // // `;
+
+  // myLibrary.forEach((b) => {
+  //   const book = document.createElement("div");
+  //   book.classList.add("book");
+  //   const name = document.createElement("p");
+  //   name.innerText = "";
+
+  //   library.appendChild(book);
+  // });
+}
