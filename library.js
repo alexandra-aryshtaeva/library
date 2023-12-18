e8bf5a22f39ae3cecd2980b809030784c347f6e2
Alexandra Aryshtaeva <alexandra.maranga@gmail.com>
2023-12-18 16:49:00 +0000

    created a book as an example to the library, started working on the dialog whne clicking in the new_book button

---
+++ library.js
@@ -0,0 +1,11 @@
+const myLibrary = [];
+
+function Book(name, author, pages) {
+  this.name = name;
+  this.author = author;
+  this.pages = pages;
+}
+
+const book1 = new Book("The Hobiit", "J.R.R. Tolkien", "320");
+
+function addBook() {}
---
+++ script.js
@@ -0,0 +1,2 @@
+const dialog = document.querySelector("dialog");
+const newBook = document.querySelector("#new_book");
